0f8f6ea648c864a8b3d41cd63c544badd749519a
Raja Subramanian <sahayu.raja@gmail.com>
2024-02-03 16:31:29 -0800

    Climate checking done

--- Climate_Check.py
+++ Climate_Check.py
@@ -1,10 +1,19 @@
-def climate_returns(season,city):
+from meteo import Weather
+
+weather=Weather("4012 Carol Avenue Fremont California")
+temperature=weather.temperature()
+precepitation=weather.percipatation()
+print(temperature)
+print(precepitation)
+def climate_returns(temperature,rainfall):
     from Climate_Data import Climate_data_spring
     from Climate_Data import Climate_data_summer
     from Climate_Data import Climate_data_winter
     from Climate_Data import Climate_data_fall
     from Climate_Data import Crop_Climate_Data
-
+    working = []
+    flag = False
+    cntr = 0
     fall_city_data = Climate_data_fall()
     summer_city_data = Climate_data_summer()
     winter_city_data = Climate_data_winter()
@@ -17,73 +26,72 @@ def climate_returns(season,city):
     winter_avg = winter_city_data.winter_data_dict
     crop_avg = crop_data.crop_data_dict
 
-    working = []
-    flag = False
-    cntr = 0
+
     
-    a = season
-    b = city
     #a = input("What is the upcoming season? Spring, Fall, Winter, or Summer?")
     #b = input("Which of the following cities are you closest to? Fremont, San Jose, Los Altos, Palo Alto, or Oakland")
-    print(a)
+    #print(a)
 
-    if a == "Fall":
-        rainfall = fall_avg[b]["Average Autumn Rainfall (inches)"]
-        temp = fall_avg[b]["Average Autumn Temperature (Fahrenheit)"]
-        for i in crop_avg:
-            flag = False
-            #print(i)
-            #crop_rain = [i]["Necessary Rainfall (inches)"]
+    #if a == "Fall":
+        #rainfall = fall_avg[b]["Average Autumn Rainfall (inches)"]
+        #temp = fall_avg[b]["Average Autumn Temperature (Fahrenheit)"]
+    for i in crop_avg:
+        flag = False
+        #print(i)
+        #crop_rain = [i]["Necessary Rainfall (inches)"]
+        #print(i,"i")
+        #print(len(crop_avg),"hahaahiahifehifheif",cntr)
+        crop_rain = crop_avg[i]["Necessary Rainfall (inches)"]
+        crop_temp = crop_avg[i]["Necessary Average Temperature (Fahrenheit)"]
+        #print(crop_rain,crop_temp,temp,rainfall)
+        if abs(crop_temp - temperature) < 15:
+            flag = True
+            print(flag)
+        if flag == True:
+            if abs(crop_rain - rainfall) < 8:
+                working.append(i)
+        cntr += 1
+    return working
+            
+'''  
+#if a == "Spring":
+#rainfall = spring_avg[b]["Average Spring Rainfall (inches)"]
+#temp = spring_avg[b]["Average Spring Temperature (Fahrenheit)"]
+#for i in crop_avg:
+#flag = False
+#print(i)
+#crop_rain = [i]["Necessary Rainfall (inches)"]
             #print(i,"i")
             #print(len(crop_avg),"hahaahiahifehifheif",cntr)
-            crop_rain = crop_avg[i]["Necessary Rainfall (inches)"]
-            crop_temp = crop_avg[i]["Necessary Average Temperature (Fahrenheit)"]
+            #crop_rain = crop_avg[i]["Necessary Rainfall (inches)"]
+            #crop_temp = crop_avg[i]["Necessary Average Temperature (Fahrenheit)"]
             #print(crop_rain,crop_temp,temp,rainfall)
-            if abs(crop_temp - temp) < 10:
-                flag = True
-            if flag == True:
-                if abs(crop_rain - rainfall) < 5:
-                    working.append(i)
-            cntr += 1
-        
-    if a == "Spring":
-        rainfall = spring_avg[b]["Average Spring Rainfall (inches)"]
-        temp = spring_avg[b]["Average Spring Temperature (Fahrenheit)"]
-        for i in crop_avg:
-            flag = False
-            #print(i)
-            #crop_rain = [i]["Necessary Rainfall (inches)"]
-            #print(i,"i")
-            #print(len(crop_avg),"hahaahiahifehifheif",cntr)
-            crop_rain = crop_avg[i]["Necessary Rainfall (inches)"]
-            crop_temp = crop_avg[i]["Necessary Average Temperature (Fahrenheit)"]
-            #print(crop_rain,crop_temp,temp,rainfall)
-            if abs(crop_temp - temp) < 10:
-                flag = True
-            if flag == True:
-                if abs(crop_rain - rainfall) < 5:
-                    working.append(i)
-            cntr += 1
+           #if abs(crop_temp - temp) < 10:
+               # flag = True
+            #if flag == True:
+                #if abs(crop_rain - rainfall) < 5:
+                    #working.append(i)
+            #cntr += 1
 
-    if a == "Summer":
-        rainfall = summer_avg[b]["Average Summer Rainfall (inches)"]
-        temp = summer_avg[b]["Average Summer Temperature (Fahrenheit)"]
-        for i in crop_avg:
-            flag = False
+    #if a == "Summer":
+        #rainfall = summer_avg[b]["Average Summer Rainfall (inches)"]
+        #temp = summer_avg[b]["Average Summer Temperature (Fahrenheit)"]
+        #for i in crop_avg:
+            #flag = False
             #print(i)
             #crop_rain = [i]["Necessary Rainfall (inches)"]
             #print(i,"i")
             #print(len(crop_avg),"hahaahiahifehifheif",cntr)
-            crop_rain = crop_avg[i]["Necessary Rainfall (inches)"]
-            crop_temp = crop_avg[i]["Necessary Average Temperature (Fahrenheit)"]
+            #crop_rain = crop_avg[i]["Necessary Rainfall (inches)"]
+            #crop_temp = crop_avg[i]["Necessary Average Temperature (Fahrenheit)"]
             #print(crop_rain,crop_temp,temp,rainfall)
-            if abs(crop_temp - temp) < 10:
-                flag = True
-            if flag == True:
-                if abs(crop_rain - rainfall) < 5:
-                    working.append(i)
-            cntr += 1
-        
+            #if abs(crop_temp - temp) < 10:
+                #flag = True
+            #if flag == True:
+                #if abs(crop_rain - rainfall) < 5:
+                    #working.append(i)
+            #cntr += 1
+   
     if a == "Winter":
         rainfall = winter_avg[b]["Average Winter Rainfall (inches)"]
         temp = winter_avg[b]["Average Winter Temperature (Fahrenheit)"]
@@ -104,5 +112,7 @@ def climate_returns(season,city):
             cntr += 1
             
     return working
-#python Climate_Check.py
-print(climate_returns("Spring","San Jose"))
+#python Climate_Check.py'''
+
+availble=climate_returns(temperature,precepitation)
+print(availble)
--- meteo.py
+++ meteo.py
@@ -1,59 +1,65 @@
 from geopy.geocoders import Nominatim
 import requests
 import json
+class Weather:
+    def __init__(self, address):
+        loc = Nominatim(user_agent="Geopy Library")
 
-loc = Nominatim(user_agent="Geopy Library")
+        # entering the location name
+        getLoc = loc.geocode(address)
 
-# entering the location name
-getLoc = loc.geocode("4012 Carol avenue Fremont California")
+        # printing address
+        print(getLoc.address)
+        self.latitude =getLoc.latitude
+        self.longitude=getLoc.longitude
 
-# printing address
-print(getLoc.address)
-latitude =getLoc.latitude
-longitude=getLoc.longitude
-# printing latitude and longitude
-#print("Latitude = ",latitude , "\n")
-#print("Longitude = ",longitude,"\n" )
+    def temperature(self):
+        
+        # printing latitude and longitude
+        #print("Latitude = ",latitude , "\n")
+        #print("Longitude = ",longitude,"\n" )
 
-url= "https://api.meteomatics.com/2024-02-03T13:00:00Z--2024-12-30T13:00:00Z:P1D/t_2m:F/"+str(latitude)+","+str(longitude)+"/json"
-#print(url)
-username = 'none_raja_sahayu'
-password = '3uxV9V9Ec1'
-response = requests.get(url, auth=(username, password))
+        url= "https://api.meteomatics.com/2024-02-03T13:00:00Z--2024-12-30T13:00:00Z:P1D/t_2m:F/"+str(self.latitude)+","+str(self.longitude)+"/json"
+        #print(url)
+        username = 'none_raja_sahayu'
+        password = '3uxV9V9Ec1'
+        response = requests.get(url, auth=(username, password))
 
-if response.status_code == 200:
-    # Print the response content
-    #print(response.text)
-    json_data=response.text
-    #print(json_data)
-    data=json.loads(json_data)
-    temperature_values = [entry['value'] for entry in data['data'][0]['coordinates'][0]['dates']]
-    # Calculate average temperature
-    average_temperature = sum(temperature_values) / len(temperature_values)
-else:
-    print(f"Error: {response.status_code} - {response.text}")
-#print(value)
+        if response.status_code == 200:
+            # Print the response content
+            #print(response.text)
+            json_data=response.text
+            #print(json_data)
+            data=json.loads(json_data)
+            temperature_values = [entry['value'] for entry in data['data'][0]['coordinates'][0]['dates']]
+            # Calculate average temperature
+            average_temperature = sum(temperature_values) / len(temperature_values)
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+        #print(value)
 
-print(average_temperature)
+        return average_temperature
+        
+    def percipatation(self):
 
-# calling the Nominatim tool and create Nominatim class
-url= "https://api.meteomatics.com/2024-02-03T13:00:00Z--2024-12-30T13:00:00Z:P1D/precip_24h:mm/"+str(latitude)+","+str(longitude)+"/json"
-#print(url)
-username = 'none_raja_sahayu'
-password = '3uxV9V9Ec1'
-response = requests.get(url, auth=(username, password))
+        url= "https://api.meteomatics.com/2024-02-03T13:00:00Z--2024-12-30T13:00:00Z:P1D/precip_24h:mm/"+str(self.latitude)+","+str(self.longitude)+"/json"
+        #print(url)
+        username = 'none_raja_sahayu'
+        password = '3uxV9V9Ec1'
+        response = requests.get(url, auth=(username, password))
 
-if response.status_code == 200:
-    # Print the response content
-    #print(response.text)
-    json_data=response.text
-    #print(json_data)
-    data=json.loads(json_data)
-    percepitation_values = [entry['value'] for entry in data['data'][0]['coordinates'][0]['dates']]
-    # Calculate average temperature
-    average_percipitation = sum(percepitation_values) / len(percepitation_values)
-else:
-    print(f"Error: {response.status_code} - {response.text}")
-#print(value)
+        if response.status_code == 200:
+            # Print the response content
+            #print(response.text)
+            json_data=response.text
+            #print(json_data)
+            data=json.loads(json_data)
+            percepitation_values = [entry['value'] for entry in data['data'][0]['coordinates'][0]['dates']]
+            # Calculate average temperature
+            average_percipitation = sum(percepitation_values) / len(percepitation_values)
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+        #print(value)
 
-print(average_percipitation)
+        print(average_percipitation)
+        return average_percipitation
